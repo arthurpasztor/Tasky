3c0240f80f2b8edf6af8c8b81e29103f0a5b66ad
arthurpasztor <apasztor.dev@gmail.com>
2024-03-29 00:44:48 +0200

    Extract executeRequest method for existing and future requests

--- app/src/main/java/com/example/tasky/auth/data/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/tasky/auth/data/AuthRepositoryImpl.kt
@@ -11,7 +11,6 @@ import com.example.tasky.auth.domain.RootError
 import com.example.tasky.auth.domain.isSuccess
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.request.post
 import io.ktor.client.request.request
 import io.ktor.client.request.setBody
 import io.ktor.client.request.url
@@ -22,44 +21,56 @@ import java.util.concurrent.CancellationException
 class AuthRepositoryImpl(private val client: HttpClient) : AuthRepository {
 
     override suspend fun login(info: LoginRequest): Result<Unit, RootError> {
-        return try {
-            val httpResponse = client.request {
-                method = HttpMethod.Post
-                url(loginUrl)
-                setBody(info)
-            }
+        return executeRequest<LoginRequest, Unit>(HttpMethod.Post, loginUrl, info) {
+            val response = it.body<TokenResponse>()
 
-            if (httpResponse.isSuccess()) {
-                val response = httpResponse.body<TokenResponse>()
+            // TODO save token to preferences
 
-                // TODO save token to preferences
-
-                Result.Success(Unit)
-            } else {
-                Result.Error(getError(httpResponse))
-            }
-        } catch (e: Exception) {
-            if (e is CancellationException) {
-                throw e
-            } else {
-                Log.e(TAG, "Error: ${e.message} / ${e.cause}")
-                Result.Error(HttpError.UNKNOWN)
-            }
+            Result.Success(Unit)
         }
     }
 
     override suspend fun signUp(info: SignUpRequest): Result<LoginRequest, RootError> {
+        return executeRequest<SignUpRequest, LoginRequest>(HttpMethod.Post, signUpUrl, info) {
+            Result.Success(LoginRequest(info.email, info.password))
+        }
+    }
+
+    private suspend inline fun <reified P, R> executeRequest(
+        httpMethod: HttpMethod,
+        url: String,
+        payload: P? = null,
+        handleResponse: (response: HttpResponse) -> Result<R, RootError>
+    ): Result<R, RootError> {
         return try {
             val httpResponse = client.request {
-                method = HttpMethod.Post
-                url(signUpUrl)
-                setBody(info)
+                method = httpMethod
+                url(url)
+                payload?.let { setBody(it) }
             }
 
             if (httpResponse.isSuccess()) {
-                Result.Success(LoginRequest(info.email, info.password))
+                handleResponse.invoke(httpResponse)
             } else {
-                Result.Error(getError(httpResponse))
+                Result.Error(
+                    when (httpResponse.status.value) {
+                        in 300..399 -> HttpError.REDIRECT
+                        401 -> HttpError.UNAUTHORIZED
+                        408 -> HttpError.REQUEST_TIMEOUT
+                        409 -> {
+                            when (url) {
+                                loginUrl -> HttpError.CONFLICT_LOGIN
+                                signUpUrl -> HttpError.CONFLICT_SIGN_UP
+                                else -> HttpError.UNKNOWN
+                            }
+                        }
+
+                        413 -> HttpError.PAYLOAD_TOO_LARGE
+                        in 400..499 -> HttpError.CLIENT_REQUEST
+                        in 500..599 -> HttpError.SERVER_RESPONSE
+                        else -> HttpError.UNKNOWN
+                    }
+                )
             }
         } catch (e: Exception) {
             if (e is CancellationException) {
@@ -70,18 +81,6 @@ class AuthRepositoryImpl(private val client: HttpClient) : AuthRepository {
             }
         }
     }
-
-    private fun getError(httpResponse: HttpResponse): RootError =
-        when (httpResponse.status.value) {
-            in 300..399 -> HttpError.REDIRECT
-            401 -> HttpError.UNAUTHORIZED
-            408 -> HttpError.REQUEST_TIMEOUT
-            409 -> HttpError.CONFLICT
-            413 -> HttpError.PAYLOAD_TOO_LARGE
-            in 400..499 -> HttpError.CLIENT_REQUEST
-            in 500..599 -> HttpError.SERVER_RESPONSE
-            else -> HttpError.UNKNOWN
-        }
 
     private val loginUrl = "${BuildConfig.BASE_URL}/login"
     private val signUpUrl = "${BuildConfig.BASE_URL}/register"
--- app/src/main/java/com/example/tasky/auth/domain/Error.kt
+++ app/src/main/java/com/example/tasky/auth/domain/Error.kt
@@ -6,7 +6,8 @@ enum class HttpError: Error {
     REDIRECT, //3xx
     UNAUTHORIZED, //401
     REQUEST_TIMEOUT, //408
-    CONFLICT, //409
+    CONFLICT_LOGIN, //409
+    CONFLICT_SIGN_UP, //409
     PAYLOAD_TOO_LARGE, //413
     CLIENT_REQUEST, //4xx
     SERVER_RESPONSE, //5xx
