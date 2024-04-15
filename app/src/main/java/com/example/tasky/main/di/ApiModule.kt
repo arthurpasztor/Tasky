b6ff26a1428f75a2a756f60299b46ab8b4e4fbb5
arthurpasztor <apasztor.dev@gmail.com>
2024-04-15 19:54:26 +0300

    Use koin's viewModelOf

--- app/src/main/java/com/example/tasky/main/di/ApiModule.kt
+++ app/src/main/java/com/example/tasky/main/di/ApiModule.kt
@@ -3,19 +3,13 @@ package com.example.tasky.main.di
 import com.example.tasky.main.data.ApiRepository
 import com.example.tasky.main.data.ApiRepositoryImpl
 import com.example.tasky.core.data.HttpClientFactory
-import com.example.tasky.core.presentation.RootViewModel
 import com.example.tasky.main.presentation.MainViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val apiModule = module {
-    viewModel {
-        MainViewModel(get(), get())
-    }
-    viewModel {
-        RootViewModel(get())
-    }
+    viewModelOf(::MainViewModel)
 
     single(named("apiClient")) { HttpClientFactory.provideApiClient() }
 
