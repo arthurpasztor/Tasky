62670ea0f2658ff4bcfb183496d044320a8dafa7
arthurpasztor <apasztor.dev@gmail.com>
2024-06-10 23:20:05 +0300

    Integrate update Task, delete Task, update Reminder and delete Reminder APIs

--- app/src/main/java/com/example/tasky/agenda/data/ReminderRepositoryImpl.kt
+++ app/src/main/java/com/example/tasky/agenda/data/ReminderRepositoryImpl.kt
@@ -14,6 +14,7 @@ import io.ktor.http.HttpMethod
 class ReminderRepositoryImpl(private val client: HttpClient) : ReminderRepository {
 
     private val reminderUrl = "${BuildConfig.BASE_URL}/reminder"
+
     override suspend fun createReminder(reminder: AgendaListItem.Reminder): Result<Unit, DataError> {
         return client.executeRequest<ReminderDTO, Unit>(
             httpMethod = HttpMethod.Post,
@@ -26,10 +27,30 @@ class ReminderRepositoryImpl(private val client: HttpClient) : ReminderRepositor
     }
 
     override suspend fun updateReminder(reminder: AgendaListItem.Reminder): Result<Unit, DataError> {
-        TODO("Not yet implemented")
+        return client.executeRequest<ReminderDTO, Unit>(
+            httpMethod = HttpMethod.Put,
+            url = reminderUrl,
+            payload = reminder.toReminderDTO(),
+            tag = TAG
+        ) {
+            Result.Success(Unit)
+        }
+    }
+
+    override suspend fun deleteReminder(reminderId: String): Result<Unit, DataError> {
+        return client.executeRequest<Unit, Unit>(
+            httpMethod = HttpMethod.Delete,
+            url = reminderUrl,
+            queryParams = Pair(QUERY_PARAM_KEY_ID, reminderId),
+            tag = TAG
+        ) {
+            Result.Success(Unit)
+        }
     }
 
     companion object {
         private const val TAG = "ReminderRepository"
+
+        private const val QUERY_PARAM_KEY_ID = "reminderId"
     }
 }
