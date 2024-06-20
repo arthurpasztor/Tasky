09f6bcfe763a70a94ba5adab11a55a325ce6a4c4
arthurpasztor <apasztor.dev@gmail.com>
2024-06-20 09:09:15 +0300

    Make AgendaDetailScreen scrollable

--- app/src/main/java/com/example/tasky/agenda/presentation/AgendaDetailScreen.kt
+++ app/src/main/java/com/example/tasky/agenda/presentation/AgendaDetailScreen.kt
@@ -12,8 +12,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CircularProgressIndicator
@@ -182,6 +184,7 @@ private fun AgendaDetailScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .background(BackgroundBlack)
     ) {
         AgendaItemDetailHeader(
